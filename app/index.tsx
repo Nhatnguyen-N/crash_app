0c2a8bcb06f0516347c78465feefb6f6b1dc1f0a
Nhatnguyen-N <nhatnguyen13213@gmail.com>
2025-01-03 18:20:54 +0700

    set up

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
-
-const index = () => {
+export default function App() {
   return (
-    <View>
-      <Text>index_abc</Text>
+    <View style={styles.container}>
+      <Text className="text-red-500 text-2xl font-pblack">Nhatt</Text>
     </View>
   );
-};
+}
 
-export default index;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
